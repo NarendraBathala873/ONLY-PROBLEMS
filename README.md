e7d8bcd07635c0c56054f08a0d0c3438f7720813
NARENDRA BATHALA <137915584+NarendraBathala873@users.noreply.github.com>
2025-04-28 00:58:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ONLY-PROBLEMS
+ a website that is full of Problems only
---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.querySelector('a').addEventListener('click', function() {
+    alert('Welcome to Only Problems! Let\'s solve some challenges together!');
+});
